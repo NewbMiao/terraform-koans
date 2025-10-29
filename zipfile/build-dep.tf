3f8dc9ea3f80a87a4673e6fb284fad6b238dc34d
newbmiao <newbvirgil@gmail.com>
2025-10-29 10:32:36 +0800

    add node runtime local build

--- zipfile/build-dep.tf
+++ zipfile/build-dep.tf
@@ -15,7 +15,7 @@ resource "null_resource" "install_deps" {
 }
 data "archive_file" "nodejs_layer" {
   type        = "zip"
-  source_dir  = "${path.module}/dep/nodejs" # Path to the layer dir
+  source_dir  = "${path.module}/dep/" # Path to the layer dir https://help.aliyun.com/zh/functioncompute/fc/user-guide/create-a-custom-layer-1?spm=a2c4g.11186623.help-menu-2838607.d_2_6_3_0.2aa15de9n35m8r&scm=20140722.H_2513609._.OR_help-T_cn~zh-V_1#p-zqv-80h-rua
   output_path = "${path.module}/dep.zip"
 
   depends_on = [null_resource.install_deps] # Wait for deps to install
